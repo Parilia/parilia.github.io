ed0366ead6c331310af57d296fabf889e47b1087
Parilia <robbiehay1@gmail.com>
2023-08-30 15:37:34 +0100

    update

--- copy.js
+++ copy.js
@@ -1,16 +1,30 @@
-function enableCopy(selector = "pre", childSelector = "code", btnText = "Copy Me", btnTextSuccess = "Copied", activeClass = "--copy") {
-    document.querySelectorAll(`${selector}:not(.${activeClass})`).forEach(node => { // create a "copy" button
- let copyBtn = document.createElement("button");
- copyBtn.innerText = btnText;
- // activeClass acts as flag so we don't add another copy button by  mistake
-  copyBtn.classList.add(activeClass);
-  node.appendChild(copyBtn);  copyBtn.addEventListener("click", async () => {    // copy to clipboard
-    if (navigator.clipboard) {
-       let text = node.querySelector(childSelector).innerText;
-       await navigator.clipboard.writeText();
-    }    // change text of button after copying
-    copyBtn.innerText = btnTextSuccess;    // change text back to normal after ### ms
-    setTimeout(() => icon.innerText = btnText, 2000);
-  })
- })
+const contentCp = document.getElementById("content-cp");
+const copyBtn = document.getElementById("copy");
+
+
+copyBtn.onclick = copyToClipboard;
+
+
+function copyToClipboard() {
+	navigator.clipboard
+		.writeText(contentCp.innerText)
+		.then(() => alert("Copied to clipboard"))
+		.catch((e) => alert(e.message));
+}
+
+
+
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
 }
